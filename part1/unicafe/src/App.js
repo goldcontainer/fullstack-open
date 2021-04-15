3907ba65bccde35f1232ad6ec69bcb1fb1d403ae
goldcontainer <joshuabkim95@gmail.com>
2021-04-14 23:33:46 -0400

    1.9 to 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,40 +8,91 @@ const Button = (props) => {
   )
 }
 
-const Statistics = (props) => {
-  if (isNaN(props.result)) {
-    return (
-      <p>{props.text} 0</p>
-    )
-  } 
-  if (props.text === 'average' || props.text === 'positive') {
-    return (
-      <p>{props.text} {props.result}%</p>
-    )
-  } 
+const Statistic = (props) => {
   return (
-    <p>{props.text} {props.result}</p> 
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  <Header header="statistics" />
+
+  if(!props.hasFeedback) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Statistic name="good" value={props.good} />
+          <Statistic name="neutral" value={props.neutral} />
+          <Statistic name="bad" value={props.bad} />
+          <Statistic name="total" value={props.total} />
+          <Statistic name="average" value={props.averagePercent + "%"} />
+          <Statistic name="positive" value={props.positivePercent + "%"} />
+        </tbody>
+      </table>
+    </div>
   )
 }
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [hasFeedback, setHasFeedback] = useState(false);
+
+  const total = good + neutral + bad;
+
+  const getPercent = (x, totalAmount) => {
+    let result = (x / totalAmount) * 100;
+
+    if(isNaN(result)) return 0;
+
+    return result;
+  };
+
+  const positivePercent = getPercent(good, total);
+
+  const getAverage = (x, y, totalAmount) => {
+    return (x - y)/totalAmount;
+  };
+
+  const averagePercent = getAverage(good, bad, total);
+
+  const buttonClick = (type) => {
+    setHasFeedback(true);
+
+    if (type === "good") {
+      setGood(good + 1);
+    } else if (type === "neutral") {
+      setNeutral(neutral + 1);
+    } else {
+      setBad(bad + 1);
+    }
+  };
+
+  const statisticsObj = {
+    hasFeedback: hasFeedback,
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: total,
+    averagePercent: averagePercent,
+    positivePercent: positivePercent,
+  };
 
   return (
     <div>
-      <Header header='give feedback'/>
-      <Button text='good' handleClick={() => setGood(good + 1)} />
-      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
-      <Button text='bad' handleClick={() => setBad(bad + 1)} />
-      <Header header='statistics' />
-      <Statistics text='good' result={good} />
-      <Statistics text='neutral' result={neutral} />
-      <Statistics text='bad' result={bad} />
-      <Statistics text='all' result={good + neutral + bad} />
-      <Statistics text='average' result={(good - bad) / (good + neutral + bad)} />
-      <Statistics text='positive' result={good / (good + neutral + bad)} />
+      <Header header="give feedback"/>
+      <Button text="good" handleClick={() => buttonClick("good")} />
+      <Button text="neutral" handleClick={() => buttonClick("neutral")} />
+      <Button text="bad" handleClick={() => buttonClick("negative")} />
+      <Statistics {...statisticsObj} />
     </div>
   )
 }
