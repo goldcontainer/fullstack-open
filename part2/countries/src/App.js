af8cb5b78d514d7b7e7cd3cbc7a3d551bf9292d0
goldcontainer <joshuabkim95@gmail.com>
2021-04-26 18:35:23 -0400

    displays languages and single country correctly

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -7,23 +7,56 @@ const Country = (props) => {
   )
 }
 
-const CountryList = (props) => {
-  return(
-    <ul>
-      {props.list.map(country =>
-        <Country key={country.id} name={country.name} />
+const CountryMatch = (props) => {
+  return (
+    <div>
+      <h1>{props.country.name}</h1>
+      <p>capital {props.country.capital}</p>
+      <p>population {props.country.population}</p>
+      <h2>languages</h2>
+      <ul>
+        {props.country.languages.map(language =>
+          <li>{language.name}</li>
         )}
-    </ul>
+      </ul>
+      <img src={props.country.flag} alt='flag'/>
+    </div>
   )
+}
+
+const CountryList = (props) => {
+  if(props.list.length > 10 && props.isFiltering) {
+    return (<p>Too many matches, specify another filter</p>)
+  } else if(!props.isFiltering) {
+    return (<div></div>)
+  } else if (props.list.length === 1) {
+    return(<CountryMatch country={props.list[0]} />)
+  } else {
+    return(
+      <ul>
+        {props.list.map(country =>
+          <Country key={country.id} name={country.name} />
+        )}
+      </ul>
+    )
+  }
 }
 
 const App = () => {
   const [ countries, setCountries ] = useState([]);
   const [ search, setSearch ] = useState('');
+  const [ isFiltering, setFilter ] = useState(false);
+
+  const searchList = countries.filter(country => country.name.toLowerCase().startsWith(search.toLowerCase()));
 
   const handleSearchChange = (event) => {
     console.log(event.target.value)
     setSearch(event.target.value)
+    if(event.target.value === '') {
+      setFilter(false);
+    } else {
+      setFilter(true);
+    }
   }
 
   useEffect(() => {
@@ -31,17 +64,19 @@ const App = () => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled');
+        // console.log('promise fulfilled');
+        // console.log(response.data);
         setCountries(response.data);
+        console.log(response.data);
       })
-  })
+  }, [])
 
-  const searchList = countries.filter(country => {country.name.toLowerCase().startsWith(search.toLowerCase())});
+  // console.log('render', countries.length, 'countries');
 
   return (
     <div>
       <div>find countries <input value={search} onChange={handleSearchChange}/></div>
-      <CountryList list={countries} />
+      <CountryList list={searchList} isFiltering={isFiltering}/>
     </div>
   )
 }
